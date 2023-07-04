eec2be4ec9cef5808cc1adf6ac2f367184b1ce12
debussysuh <debussysuh@gmail.com>
2023-07-04 21:32:15 +0900

    Initial Commit

---
+++ src/test/java/com/example/vblogserver/VblogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vblogserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VblogServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
