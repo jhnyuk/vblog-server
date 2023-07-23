7ac4fe088ac61a752ef8dfc71bb1dc6dc08da0e4
mongdolkim <wn96dud@m365.dongyang.ac.kr>
2023-07-23 21:17:39 +0900

    초기 커밋

---
+++ src/main/java/com/example/vblogserver/VblogServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.vblogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VblogServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VblogServerApplication.class, args);
+	}
+
+}
