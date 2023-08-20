68ef6b5578099a1c362650c8649916d6dcb8b0b2
mongdolkim <wn96dud@m365.dongyang.ac.kr>
2023-08-20 20:43:30 +0900

    feat : Review Entity 에 createdDate 타입 변경(String -> LocalDateTime), 리뷰 작성자(user) 추가 #6

--- src/main/java/com/example/vblogserver/domain/review/entity/Review.java
+++ src/main/java/com/example/vblogserver/domain/review/entity/Review.java
@@ -1,12 +1,16 @@
 package com.example.vblogserver.domain.review.entity;
 
 import com.example.vblogserver.domain.board.entity.Board;
+import com.example.vblogserver.domain.category.entity.CategoryS;
+import com.example.vblogserver.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -20,15 +24,28 @@ public class Review {
     private String content;
     @CreatedDate
     @Column(name = "CREATED_DATE", updatable = false, nullable = false)
-    private String createdDate;
+    private LocalDateTime createdDate;
 
+    //게시글 작성자
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id")
     private Board board;
 
+    // 리뷰 작성자
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
     @Builder
-    public Review(String content, Board board) {
+    public Review(String content, Board board, User user) {
         this.content = content;
         this.board = board;
+        this.user = user;
+    }
+
+    //댓글 작성 시간을 서버의 시간으로 insert
+    @PrePersist
+    public void prePersist(){
+        createdDate = LocalDateTime.now();
     }
 }
