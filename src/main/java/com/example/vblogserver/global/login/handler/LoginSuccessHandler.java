8813adb14b1250d9e4523c29ca3cb483a9403c76
JHNYUK <118197952+debussykim@users.noreply.github.com>
2023-09-10 08:42:21 +0900

    chore: 로그인 시 응답 데이터에 imageUrl 추가 (#23)

--- src/main/java/com/example/vblogserver/global/login/handler/LoginSuccessHandler.java
+++ src/main/java/com/example/vblogserver/global/login/handler/LoginSuccessHandler.java
@@ -1,5 +1,9 @@
 package com.example.vblogserver.global.login.handler;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,6 +11,8 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationSu
 
 import com.example.vblogserver.domain.user.repository.UserRepository;
 import com.example.vblogserver.global.jwt.service.JwtService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -24,18 +30,36 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-		Authentication authentication) {
+		Authentication authentication) throws IOException {
 		String loginId = extractUsername(authentication); // 인증 정보에서 Username(loginId) 추출
 		String accessToken = jwtService.createAccessToken(loginId); // JwtService의 createAccessToken을 사용하여 AccessToken 발급
 		String refreshToken = jwtService.createRefreshToken(); // JwtService의 createRefreshToken을 사용하여 RefreshToken 발급
 
-		jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken); // 응답 헤더에 AccessToken, RefreshToken 실어서 응답
-
 		userRepository.findByLoginId(loginId)
 			.ifPresent(user -> {
 				user.updateRefreshToken(refreshToken);
-				userRepository. saveAndFlush(user);
+				userRepository.saveAndFlush(user);
+
+				// 응답 헤더에 AccessToken, RefreshToken 실어서 응답
+				jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
+
+				Map<String, Object> responseBody = new HashMap<>();
+
+				responseBody.put("imageUrl", user.getImageUrl());
+				responseBody.put("username", user.getUsername());
+
+				Gson gson = new GsonBuilder().serializeNulls().create();
+				String jsonResponseBody = gson.toJson(responseBody);
+
+				response.setContentType("application/json;charset=UTF-8");
+
+				try {
+					response.getWriter().write(jsonResponseBody);
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
 			});
+
 		log.info("로그인에 성공하였습니다. Id: {}", loginId);
 		log.info("로그인에 성공하였습니다. AccessToken: {}", accessToken);
 		log.info("발급된 AccessToken 만료 기간: {}", accessTokenExpiration);
