954c4ba676e6336e2762316496432275607787d1
mongdolkim <wn96dud@m365.dongyang.ac.kr>
2023-09-10 19:35:09 +0900

    수정

--- src/main/java/com/example/vblogserver/init/tmp/TmpLikeCount.java
+++ src/main/java/com/example/vblogserver/init/tmp/TmpLikeCount.java
@@ -4,16 +4,17 @@ import com.example.vblogserver.domain.board.entity.Board;
 import com.example.vblogserver.domain.board.repository.BoardRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
 @Component
-public class TmpLikeCount implements CommandLineRunner {
+public class TmpLikeCount{
     private final BoardRepository boardRepository;
 
     public TmpLikeCount(BoardRepository boardRepository) {
         this.boardRepository = boardRepository;
     }
 
-    @Override
-    public void run(String... args) throws Exception {
+
+    public void updateBoardLikeCount() {
         Board board = boardRepository.findById(5L).orElse(null);
         board.setLikeCount(30);
         boardRepository.save(board);
