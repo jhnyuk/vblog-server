acb690cc48fc2a15a4bc9afd147fccffc0128525
JHNYUK <118197952+debussykim@users.noreply.github.com>
2023-09-11 21:10:13 +0900

    chore: testuser2 로그인되도록 수정 (#26)

--- src/main/java/com/example/vblogserver/init/tmp/TmpUser.java
+++ src/main/java/com/example/vblogserver/init/tmp/TmpUser.java
@@ -1,28 +1,34 @@
 package com.example.vblogserver.init.tmp;
 
+import com.example.vblogserver.domain.user.entity.Role;
 import com.example.vblogserver.domain.user.entity.User;
 import com.example.vblogserver.domain.user.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 
 @Component
 public class TmpUser implements CommandLineRunner {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public TmpUser(UserRepository userRepository) {
+    public TmpUser(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void run(String... args) throws Exception {
         User testUser = User.builder()
-                .loginId("testuser2")
-                .password("Test123!pw")
-                .username("thisistest")
-                .imageUrl("https://example.com/profile.jpg")
-                .socialId(null)
-                .build();
+            .loginId("testuser2")
+            .password("Test123!pw")
+            .username("thisistest")
+            .imageUrl("https://example.com/profile.jpg")
+            .role(Role.USER)
+            .socialId(null)
+            .build();
+        testUser.passwordEncode(passwordEncoder);
         userRepository.save(testUser);
     }
 }
