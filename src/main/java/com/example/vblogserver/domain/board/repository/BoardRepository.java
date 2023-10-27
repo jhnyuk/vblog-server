ebfa176acfff7c5a350507d7f23eccaaac7cfbc3
mongdolkim <wn96dud@m365.dongyang.ac.kr>
2023-10-27 16:12:06 +0900

    feat : 사용자 추천 게시글 조회 API 기능 구현

--- src/main/java/com/example/vblogserver/domain/board/repository/BoardRepository.java
+++ src/main/java/com/example/vblogserver/domain/board/repository/BoardRepository.java
@@ -37,5 +37,5 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     List<Board> findByFolder(Folder folder);
     // 유저가 즐겨찾는 카테고리에 해당되는 게시글 조회
-    List<Board> findByCategoryM(CategoryM categoryM);
+    List<Board> findByCategoryGAndCategoryM(CategoryG categoryG, CategoryM categoryM);
 }
